2e6e51bbd0685c0c9bb0cfc0625f83d24084ff28
rvale01 <rvale09@gmail.com>
2023-01-06 23:24:37 +0000

    fix be

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,15 +1,16 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import cors from 'cors'
 
-dotenv.config();
+const app = express()
+const port = 5000
 
-const app: Express = express();
-const port = 5454;
+app.use(cors({ origin: 'http://localhost:3000' }))
 
-app.get('/hello', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+app.get('/workspaces', (_, response) => {
+  const workspaces = [
+    { name: 'api', version: '1.0.0' },
+    { name: 'types', version: '1.0.0' },
+    { name: 'web', version: '1.0.0' },
+  ]
+  response.json({ data: workspaces })
+})
