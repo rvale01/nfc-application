a2fab5d17d248e90638b8336072bcb4c9648f310
rvale01 <rvale09@gmail.com>
2023-01-07 16:59:33 +0000

    forcing build

--- apps/web-app/src/pages/General/Home.tsx
+++ apps/web-app/src/pages/General/Home.tsx
@@ -40,13 +40,12 @@ const View3 = () => {
                 <Text 
                     textAlign="center"
                     text="You will be able to keep up with all your appointments from one place. Subscribe to our newsletter to be the first to try it out!" 
-                    fontWeight="bold" size='small' color="white"
+                    fontWeight="bold" size='small' color="link" //TODO: change to white
                 />
             </Box>
         </Box>
     )
 }
-
 
 export const Home = () => {
     return(
