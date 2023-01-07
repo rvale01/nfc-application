18cf5d8ff3d4113f39311ee86495fc7d51bbe503
rvale01 <rvale09@gmail.com>
2023-01-07 19:08:25 +0000

    updated fe general

--- apps/web-app/src/pages/General/index.tsx
+++ apps/web-app/src/pages/General/index.tsx
@@ -5,31 +5,32 @@ import {
   } from "react-router-dom";
 import { Home } from "./Home";
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-    {
-        path: "/login",
-        element: <div>Hello world!</div>,
-    },
-    {
-        path: "/register",
-        element: <div>Hello world!</div>,
-    },
-    {
-        path: "/doctor",
-        element: <div>Hello world!</div>,
-    },
-    {
-        path: "/patient",
-        element: <div>Hello world!</div>,
-    },
-  ]);
+// const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Home/>,
+//     },
+//     {
+//         path: "/login",
+//         element: <div>Hello world!</div>,
+//     },
+//     {
+//         path: "/register",
+//         element: <div>Hello world!</div>,
+//     },
+//     {
+//         path: "/doctor",
+//         element: <div>Hello world!</div>,
+//     },
+//     {
+//         path: "/patient",
+//         element: <div>Hello world!</div>,
+//     },
+//   ]);
 
 export const General = () => {
     return(
-        <RouterProvider router={router} />
+        <div>ok</div>
+        // <RouterProvider router={router} />
     )
 }
