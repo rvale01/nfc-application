b11a43a1ef46890852a7f219b38011181c7b015a
rvale01 <rvale09@gmail.com>
2023-01-07 16:54:23 +0000

    fixed docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "ui-web";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
+      <Button label="okok" onClick={()=> console.log("ok")}/>
     </div>
   );
 }
